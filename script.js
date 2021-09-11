b7cf5da1c89af06600489cfcc61191c87b7a0347
salvinanto7 <salvinanto7@gmail.com>
2021-09-12 00:13:13 +0530

    set up whole functinality except the themes

--- script.js
+++ script.js
@@ -42,10 +42,90 @@ setInterval(()=>{
 
 },1000);
 
+get_weather_data();
+
 function get_weather_data(){
     navigator.geolocation.getCurrentPosition((success)=>{
-        let (lat,long) = success.coords;
+        let {latitude,longitude} = success.coords;
 
-        fetch(`https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${long}&exclude=hourly,miutely&appid=${api_key}`).then(res = res.json).then()
+        fetch(`https://api.openweathermap.org/data/2.5/onecall?lat=${latitude}&lon=${longitude}&exclude=hourly,miutelys&units=metric&appid=${api_key}`).then(res => res.json()).then(data=>{
+        
+        console.log(data);
+        updateWeatherData(data);
+        })
     })
 }
+
+function updateWeatherData(data){
+    let{humidity,pressure,temp,visibility,wind_speed}=data.current;
+    const temp1 = document.getElementById('temp-1');
+    const pressure1 = document.getElementById('pressure-1');
+    const humidity1 = document.getElementById('humidity-1');
+    const visibility1 = document.getElementById('visibility-1');
+    const windSpeed1 = document.getElementById('wind-speed-1');
+    const location = document.getElementById('location');
+    
+    temp1.innerHTML = `<span>Temperature</span>  &nbsp; &nbsp; ${temp}&#176C`;
+    pressure1.innerHTML = `<span>Pressure</span>  &nbsp; &nbsp; ${pressure} mb`;
+    humidity1.innerHTML = `<span>Humidity</span>  &nbsp; &nbsp; ${humidity}%`;
+    windSpeed1.innerHTML = `<span>Wind Speed</span> &nbsp; &nbsp; ${wind_speed} km/h`
+    location.innerHTML = data.timezone;
+    
+    let otherforecast='';
+    let tmrwWeatherForecast='';
+    data.daily.forEach((day,idx) => {
+        if(idx==0 || idx==7){
+
+        }
+        else if(idx==1){
+            
+            const tmrwtime = new Date(new Date().getTime()+24*60*60*1000);
+            const tmrwMonth = tmrwtime.getMonth();
+            const tmrwDay = tmrwtime.getDay();
+            const tmrwDate = tmrwtime.getDate();
+            const tmrwYear = tmrwtime.getFullYear();
+
+            tmrwWeatherForecast+=`
+            <div class="tmrw-temp" id="tmrw-temp">
+                <div class="temp">
+                    <p class="temp"> ${day.temp.day}&#176C</p>
+                    <img src="./images/rainy-day.png" alt="">
+                </div>
+                <div class="date">
+                    <p id=tmrw-date>${tmrwDate}th ${months[tmrwMonth]} ${tmrwYear}</p>
+                    <p id="tmrw-day">${days[tmrwDay]} &nbsp; Tomorrow</p>
+                </div>
+            </div>
+            <div class="tmrw-info weather">
+                <p id="pressure"> <span>Pressure</span> &nbsp; &nbsp; 1010mb</p>
+                    <p id="humidity"><span>Humidity</span>  &nbsp; &nbsp; ${day.humidity}%</p>
+                    <p id="aiq"><span>Wind Speed</span> &nbsp; &nbsp; 3 km/h</p>
+            </div>
+            `
+        }
+        else{
+            otherforecast+=`
+            <div class="week-day">
+                <h3>${days[idx]}</h3>
+                <div class="tmrw-temp">
+                    <p class="temp"> ${day.temp.day}&#176C</p>
+                    <img src="./images/rainy-day.png" alt="">
+                </div>
+                <div class="tmrw-info weather">
+                    <p id="pressure"> <span>Pressure</span> &nbsp; &nbsp; ${day.pressure}mb</p>
+                    <p id="humidity"><span>Humidity</span>  &nbsp; &nbsp; ${day.humidity}%</p>
+                    <p id="aiq"><span>Wind Speed</span> &nbsp; &nbsp; ${day.wind_speed} km/h</p>
+                </div>
+            </div>
+            `
+        }
+        
+    });
+    const weeekForecast = document.getElementById('week-foreccast');
+
+    weeekForecast.innerHTML=otherforecast;
+
+    const tmrwForecast = document.getElementById('tmrw-forecast');
+    tmrwForecast.innerHTML = tmrwWeatherForecast;
+}
+
